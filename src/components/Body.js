705fe59e77419954fb04304ba25ac38008fd2911
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-02-09 17:59:47 +0530

    added

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,41 +4,7 @@ import resList from "../utils/mockData";
 
 const Body = () => {
   // Local State Variable - Super powerful variable
-  const [listOfRestaurants, setListOfRestaurants] = useState([
-    {
-      data: {
-        id: "121602",
-        name: "Kannur Food Point",
-        cloudinaryImageId: "bmwn4n4bn6n1tcpc8x2h",
-        cuisines: ["Kerala", "Chinese"],
-        costForTwo: 30000,
-        deliveryTime: 24,
-        avgRating: "3.9",
-      },
-    },
-    {
-      data: {
-        id: "121603",
-        name: "Dominos",
-        cloudinaryImageId: "bmwn4n4bn6n1tcpc8x2h",
-        cuisines: ["Kerala", "Chinese"],
-        costForTwo: 30000,
-        deliveryTime: 24,
-        avgRating: "4.5",
-      },
-    },
-    {
-      data: {
-        id: "121604",
-        name: "MCD",
-        cloudinaryImageId: "bmwn4n4bn6n1tcpc8x2h",
-        cuisines: ["Kerala", "Chinese"],
-        costForTwo: 30000,
-        deliveryTime: 24,
-        avgRating: "4.1",
-      },
-    },
-  ]);
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
   // Normal js variable
   // let listOfRestaurants = null;
 
