ff3d5a5a969ececfd9f8f59d6986836037aad2a0
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-02-12 15:34:10 +0530

    added

--- src/components/User.js
+++ src/components/User.js
@@ -2,9 +2,11 @@ import { useState } from "react";
 
 const User = ({ name }) => {
   const [count] = useState(0);
+  const [count2] = useState(1);
   return (
     <div className="user-card">
       <h1>count = {count}</h1>
+      <h1>count = {count2}</h1>
       <h2>Name: {name}</h2>
       <h2>Location: Zaid</h2>
       <h2>Contact: TechTinterZaid</h2>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -5,16 +5,20 @@ class UserClass extends React.Component {
     super(props);
     this.state = {
       count: 0,
+      count2: 2,
     };
   }
 
   render() {
     const { name, location } = this.props;
+    const { count, count2 } = this.state;
+
     // return some piece of jsx
 
     return (
       <div className="user-card">
-        <h1>count = {this.state.count}</h1>
+        <h1>count = {count}</h1>
+        <h1>count = {count2}</h1>
         <h2>Name: {name}</h2>
         <h2>Location: {location}</h2>
         <h2>Contact: TechTinterZaid</h2>
