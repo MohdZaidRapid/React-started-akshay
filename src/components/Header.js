77dbdbbb9dc1269051a7b4ed454b47ca7e47ecf7
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-02-14 16:26:47 +0530

    configure  tailwind-react-

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,27 +16,27 @@ const Header = () => {
   // }, [btnNameReact]);
 
   return (
-    <div className="header">
+    <div className="flex justify-between bg-pink-100 shadow-lg">
       <div className="logo-container">
-        <img src={LOGO_URL} alt="App Logo" className="logo" />
+        <img src={LOGO_URL} alt="App Logo" className="w-48" />
       </div>
-      <div className="nav-items">
-        <ul>
-          <li>Online Status: {onlineStatus ? "✅" : "🔴"}</li>
-          <li>
+      <div className="nav-items flex items-center">
+        <ul className="flex p-4 m-4 ">
+          <li className="px-4">Online Status: {onlineStatus ? "✅" : "🔴"}</li>
+          <li className="px-4">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/about">About Us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/contact">Contact Us</Link>
           </li>
-          <li>
+          <li className="px-4">
             <Link to="/grocery">Grocery </Link>
           </li>
 
-          <li>Cart</li>
+          <li className="px-4">Cart</li>
           <button
             className="login"
             onClick={() => {
