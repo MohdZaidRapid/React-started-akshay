f92f8fc9d737fa8f0f61d816e67c0ad3787a6c25
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-02-15 08:08:34 +0530

    added

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,3 +1,5 @@
+import { CDN_URL } from "../utils/constants";
+
 const ItemList = ({ items }) => {
   console.log(items);
   return (
@@ -7,9 +9,15 @@ const ItemList = ({ items }) => {
           key={item.card.info.id}
           className="p-2 m-2  border-b-2 border-gray-200 text-left"
         >
-          <div className="p-2">
+          <div className="py-2">
+            <img src={CDN_URL + item.card.info.imageId} className="w-14" />
             <span> {item.card.info.name}</span>
-            <span> - ₹ {item.card.info.price / 100}</span>
+            <span>
+              - ₹
+              {item.card.info.price
+                ? item.card.info.price / 100
+                : item.card.info.defaultPrice / 100}
+            </span>
           </div>
           <p className="text-xs">{item.card.info.description}</p>
         </div>
