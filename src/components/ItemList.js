c715a4453c3cc07e2b506733e6018ee7b9788cab
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-02-15 08:19:57 +0530

    added

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -7,19 +7,23 @@ const ItemList = ({ items }) => {
       {items.map((item) => (
         <div
           key={item.card.info.id}
-          className="p-2 m-2  border-b-2 border-gray-200 text-left"
+          className="p-2 m-2  border-b-2 border-gray-200 text-left flex justify-between"
         >
-          <div className="py-2">
-            <img src={CDN_URL + item.card.info.imageId} className="w-14" />
-            <span> {item.card.info.name}</span>
-            <span>
-              - ₹
-              {item.card.info.price
-                ? item.card.info.price / 100
-                : item.card.info.defaultPrice / 100}
-            </span>
+          <div>
+            <div className="py-2">
+              <span> {item.card.info.name}</span>
+              <span>
+                - ₹
+                {item.card.info.price
+                  ? item.card.info.price / 100
+                  : item.card.info.defaultPrice / 100}
+              </span>
+            </div>
+            <p className="text-xs">{item.card.info.description}</p>
           </div>
-          <p className="text-xs">{item.card.info.description}</p>
+          <div className="w-40 p-4">
+            <img src={CDN_URL + item.card.info.imageId} />
+          </div>
         </div>
       ))}
     </div>
