878e15edb8328f282dd15b9d72133b3bcc39db13
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-02-28 16:01:20 +0530

    added test case

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -3,6 +3,21 @@ import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
 describe("Contact Us Page Test Case", () => {
+  beforeAll(() => {
+    console.log("before All the test");
+  });
+
+  beforeEach(() => {
+    console.log("Before each");
+  });
+
+  afterAll(() => {
+    console.log("After all");
+  });
+
+  afterEach(() => {
+    console.log("After Each");
+  });
   it("Should load contact us component", () => {
     render(<Contact />);
 
